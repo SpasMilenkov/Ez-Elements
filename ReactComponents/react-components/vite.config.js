791ebaa4d363610c90d6e80e44c3b91e77a74067
Shredrox <93082689+Shredrox@users.noreply.github.com>
2023-07-27 16:03:21 +0300

    Added initial react project

---
+++ ReactComponents/react-components/vite.config.js
@@ -0,0 +1,28 @@
+import { resolve } from 'node:path'
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+
+import * as packageJson from './package.json'
+// https://vitejs.dev/config/
+
+export default defineConfig(() => ({
+  plugins: [
+    react()
+  ],
+  define: {
+    'process.env': {},
+  },
+  build: {
+    lib: {
+      entry: resolve('src', 'index.js'),
+      name: 'ReactViteLibrary',
+      formats: ['es', 'cjs', 'umd'],
+      fileName: (format) => `react-vite-library.${format}.js`,
+    },
+    manifest: true,
+    rollupOptions: {
+      external: [...Object.keys(packageJson.peerDependencies)],
+    },
+  }
+}))
+
