cba6c935f56984b32951e1de2b984adb8a7edfec
Spas Milenkov <spasmilenkov123@gmail.com>
2023-08-22 17:14:03 +0300

    Add "Oh, Vue Icons!" to the project

--- VueComponents/Ez-Elements-Vue/src/main.ts
+++ VueComponents/Ez-Elements-Vue/src/main.ts
@@ -1,5 +1,31 @@
 import { createApp } from 'vue'
-import './style.css'
+import "./assets/styles.css"
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import { OhVueIcon, addIcons } from "oh-vue-icons";
+
+import { 
+    BiFacebook,
+    BiInstagram,
+    BiLinkedin,
+    BiTwitter,
+    BiTelephoneFill,
+    IoLocationSharp,
+    //TODO: swap the icon because it doesnt work with color props
+    IoGlobeOutline,
+    IoMail 
+} from "oh-vue-icons/icons";
+
+addIcons(BiFacebook, 
+    BiInstagram,
+    BiLinkedin,
+    BiTwitter,
+    BiTelephoneFill,
+    IoLocationSharp,
+    IoGlobeOutline,
+    IoMail)
+
+const app =createApp(App)
+
+app.component('v-icon', OhVueIcon)
+app.mount('#app')
