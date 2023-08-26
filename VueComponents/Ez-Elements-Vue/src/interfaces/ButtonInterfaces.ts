1d9014f9fb3af319792f7bcf75e92c09a2bd45cc
Spas Milenkov <spasmilenkov123@gmail.com>
2023-08-26 21:35:53 +0300

    Format and adjust interfaces

--- VueComponents/Ez-Elements-Vue/src/interfaces/ButtonInterfaces.ts
+++ VueComponents/Ez-Elements-Vue/src/interfaces/ButtonInterfaces.ts
@@ -1,13 +1,15 @@
 export interface ButtonSettings {
-    content: string,
-    background?: string,
-    fontFamily?: string,
-    color?: string,
-    radiusPreset?: RadiusPreset,
-    customRadius?: string,
-    shadow?: string,
-    fontSize?: string,
-    border?: string,
-    borderFocused?: string,
-    action: () => any
+    background?: string;
+    fontFamily?: string;
+    color?: string;
+    radiusPreset?: RadiusPreset;
+    customRadius?: string;
+    shadow?: string;
+    fontSize?: string;
+    border?: string;
+    borderFocused?: string;
+}
+export interface ButtonData {
+    content: string;
+    action?: () => any;
 }
--- VueComponents/Ez-Elements-Vue/src/interfaces/CardInterfaces.ts
+++ VueComponents/Ez-Elements-Vue/src/interfaces/CardInterfaces.ts
@@ -1,30 +1,30 @@
 export interface CardSettings {
-    subtitleColor?: string,
-    subtitleFont?: string,
-    subtitleSize?: string,
-    detailsColor?: string,
-    detailsFont?: string,
-    detailsSize?: string,
-    pictureBorder?: string,
-    cardBackground?: string,
-    radiusPreset?: RadiusPreset,
+    subtitleColor?: string;
+    subtitleFont?: string;
+    subtitleSize?: string;
+    detailsColor?: string;
+    detailsFont?: string;
+    detailsSize?: string;
+    pictureBorder?: string;
+    cardBackground?: string;
+    radiusPreset?: RadiusPreset;
     infoIcons?: {
-        iconFillColor?: string,
-        multipleColors: string[],
-        iconSize?: number
-    },
+        iconFillColor?: string;
+        multipleColors: string[];
+        iconSize?: number;
+    };
     mediaIcons?: {
-        iconFillColor?: string,
-        multipleColors: string[],
-        iconSize?: number
-    }
+        iconFillColor?: string;
+        multipleColors: string[];
+        iconSize?: number;
+    };
 }
 export interface CardData {
-    userName: string,
-    userPicture: string,
-    userBanner?: string,
-    userInfo: string[],
-    userMedia: string[],
-    infoIcons: string[],
-    mediaIcons: string[]
+    userName: string;
+    userPicture: string;
+    userBanner?: string;
+    userInfo: string[];
+    userMedia: string[];
+    infoIcons: string[];
+    mediaIcons: string[];
 }
