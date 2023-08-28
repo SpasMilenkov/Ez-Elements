4fae570acb9c73bccfb28678b192841ca830d31f
Spas Milenkov <spasmilenkov123@gmail.com>
2023-08-28 22:21:52 +0300

    Add grid for better presentation

--- VueComponents/Ez-Elements-Vue/src/App.vue
+++ VueComponents/Ez-Elements-Vue/src/App.vue
@@ -1,30 +1,49 @@
 <template>
-    <MainButton
-        :data="{
-            content: 'Test',
-        }"
-        :settings="{
-            color: 'black',
-        }"
-        @click="togglePopup"
-    />
-    <SecondaryButton
-        :data="{
-            content: 'Test',
-        }"
-    />
-    <UserVerticalCard />
-    <UserHorizontalCard />
-    <ActionCard
-        :data="{
-            cardTitle: 'My Card',
-            cardContent: 'My epic popup i guess',
-        }"
-        v-if="showPopup"
-        @close-popup="showPopup = false"
-    />
-    <TallFormComponent ></TallFormComponent>
-<!-- 
+    <main class="grid">
+            <div :class="$style.flex">
+                <MainButton
+                    :data="{
+                        content: 'Test',
+                    }"
+                    :settings="{
+                        color: 'black',
+                    }"
+                    @click="togglePopup"
+                />
+                <SecondaryButton
+                    :data="{
+                        content: 'Test',
+                    }"
+                />
+            </div>
+            <div :class="$style.flex">
+                <UserVerticalCard />
+            </div>
+            <div :class="$style.flex">
+                <UserHorizontalCard />
+            </div>
+            <div :class="$style.flex">
+                <ActionCard
+                    :data="{
+                        cardTitle: 'My Card',
+                        cardContent: 'My epic popup i guess',
+                    }"
+                    v-if="showPopup"
+                    @close-popup="showPopup = false"
+                />
+            </div>
+            <div :class="$style.flex">
+                <TallFormComponent />
+            </div>
+            <div :class="$style.flex">
+                <TestimonialCard />
+            </div>
+            <div :class="$style.flex">
+                <TallTestimonialCard />
+            </div>
+    </main>
+
+    <!-- 
     Photo by
     <a
         href="https://unsplash.com/@podpros?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"
@@ -33,8 +52,11 @@
     on
     <a
         href="https://unsplash.com/photos/GEnCnYhA1J4?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"
-        >Unsplash</a
-    > -->
+        >Unsplash</a    
+    > 
+    Photo by <a href="https://unsplash.com/@contentbyemile?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Emile</a> on <a href="https://unsplash.com/photos/ANKHSrwLhWU?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+        
+    -->
 </template>
 
 <script setup lang="ts">
@@ -45,10 +67,20 @@ import ActionCard from './components/ActionCardComponent.vue';
 import UserHorizontalCard from './components/UserHorizontalCardComponent.vue';
 import SecondaryButton from './components/SecondaryButtonComponent.vue';
 import TallFormComponent from './components/FormComponent.vue';
+import TestimonialCard from './components/TestimonialCardComponent.vue';
+import TallTestimonialCard from './components/TallTestimonialCardComponent.vue';
+
 const showPopup = ref(false);
 
 function togglePopup() {
     showPopup.value = true;
 }
 </script>
-<style scoped></style>
+<style module>
+.flex{
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 1rem;
+}
+</style>
