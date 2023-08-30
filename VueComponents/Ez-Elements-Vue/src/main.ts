176d9fcf9e7a4e089d27d861fc04365d98e7568b
Spas Milenkov <spasmilenkov123@gmail.com>
2023-08-30 21:43:13 +0300

    Add router config

--- VueComponents/Ez-Elements-Vue/src/main.ts
+++ VueComponents/Ez-Elements-Vue/src/main.ts
@@ -2,8 +2,10 @@ import { createApp } from 'vue'
 import "./assets/styles.css"
 import App from './App.vue'
 import { Icon } from '@iconify/vue'
+import router from './router/index'
+
 const app =createApp(App)
 app.component('Icon', Icon)
-
+app.use(router)
 app.mount('#app')
         
