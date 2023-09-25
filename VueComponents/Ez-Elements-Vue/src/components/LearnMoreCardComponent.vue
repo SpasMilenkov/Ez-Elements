06ca55613486e0e848e9945c66951d50562b6b54
Spas Milenkov <spasmilenkov123@gmail.com>
2023-09-25 20:22:09 +0300

    Add "learn more" card structure

---
+++ VueComponents/Ez-Elements-Vue/src/components/LearnMoreCardComponent.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="card-body">
+        <div class="flex-column">
+            <h1 class="card-heading"></h1>
+            <p class="card-text"></p>
+            <mainButton></mainButton>
+        </div>
+        <picture>
+            <img src="" alt="" class="info-image">
+        </picture>
+    </div>
+</template>
+<script setup lang='ts'>
+import mainButton from './MainButtonComponent.vue';
+
+
+</script>
+<style scoped></style>
